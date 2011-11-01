a31ee5eecee441410c69935f4ebf6bffa5d6089c
Patrick Golden <ptgolden@berkeley.edu>
2011-11-01 14:33:23 -0700

    More work on attachments, styles

--- editorsnotes/tasks/views.py
+++ editorsnotes/tasks/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 import json
-from models import Task, Comment
+from models import Task, Comment, AttachmentAssignment
+from editorsnotes.main.models import Note, Topic, Document
 
 @login_required
 def task(request, task_id):
     o = {}
     o['task'] = Task.objects.get(id=task_id)
+    o['user'] = request.user
     return render_to_response(
         'task.html', o, context_instance=RequestContext(request))
 
@@ -19,7 +21,6 @@ def all_tasks(request):
         'all-tasks.html', o, context_instance=RequestContext(request))
 
 def add_comment(request, task_id):
-    o = {}
     u = request.user
     message = request.POST.get('comment-text', '')
     attachments = request.POST.getlist('attachment')
@@ -28,9 +29,27 @@ def add_comment(request, task_id):
                                    task=Task.objects.get(id=task_id),
                                    text=message)
         c.save()
-        o['status'] = 'ok'
-        o['new_comment'] = c.text
     else:
-        o['status'] = 'broke'
-    raise Exception
-    return HttpResponse(json.dumps(o), mimetype='text/plain')
+        #TODO: blank comments
+        pass
+    for attachment in attachments:
+        if not attachment:
+            continue
+        attachment_type = attachment.split(' ')[0]
+        attachment_id = int(attachment.split(' ')[1])
+        if attachment_type == 'notes':
+            o = Note.objects.get(id=attachment_id)
+        elif attachment_type == 'documents':
+            o = Document.objects.get(id=attachment_id)
+        elif attachment_type == 'topics':
+            o = Topic.objects.get(id=attachment_id)
+        else:
+            continue
+        assignment = AttachmentAssignment.objects.create(
+            content_object = o,
+            comment = c,
+            creator = request.user
+            )
+        assignment.save()
+    return_url = request.GET.get('return_to', '/')
+    return HttpResponseRedirect(return_url)
