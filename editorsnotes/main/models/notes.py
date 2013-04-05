04d5348a0af3e7d561b84e01a716038d80434802
Patrick Golden <ptgolden0@gmail.com>
2013-04-05 15:09:34 -0700

    Try again at NoteSection model
    In b8cac7a I got rid of note sections in favor of just using citations.
    At the time, I said that if we wanted to go back to note sections, "we
    can make [different types of sections] into their own models, and have
    note sections be a generic foreign key to them". That's what this commit
    does, except using concrete inheritence instead of generic
    relationships.
    
    The three types of note sections I've included:
    	* Citations
    	* Text
    	* references to other notes
    
    These distinct sections are all stored in different tables, but in most
    cases, we will want to retrieve them by getting all of a notes'
    sections, regardless of their subclasses. To accomplish this without
    hitting the database much, I've added django-model-utils as a
    requirement. It includes a manager for such a use case. See the article
    "Automatic Downcasting of Inherited Models in Django" [1] for the basis
    of this manager.
    
    An advantage of this split is that we will be able to add different
    types of sections in a modular way. Next I'll need to make serializers
    and templates for each type of new section.
    
    [1] http://jeffelmore.org/2010/11/11/automatic-downcasting-of-inherited-models-in-django/

--- editorsnotes/main/models/notes.py
+++ editorsnotes/main/models/notes.py
@@ -1,9 +1,17 @@
 from django.contrib.contenttypes import generic
 from django.db import models
+from lxml import etree
+from model_utils.managers import InheritanceManager
 
 from .. import fields
 from base import (Administered, LastUpdateMetadata, ProjectSpecific,
                   URLAccessible)
+
+NOTE_STATUS_CHOICES = (
+    ('0', 'Closed'),
+    ('1', 'Open'),
+    ('2', 'Hibernating')
+)
 
 class Note(LastUpdateMetadata, Administered, URLAccessible, ProjectSpecific):
     u""" 
@@ -11,17 +19,13 @@ class Note(LastUpdateMetadata, Administered, URLAccessible, ProjectSpecific):
     so it may have hyperlinks and all the other features that XHTML
     enables.
     """
-    NOTE_STATUS_CHOICES = (
-        ('0', 'Closed'),
-        ('1', 'Open'),
-        ('2', 'Hibernating')
-    )
     title = models.CharField(max_length='80', unique=True)
     content = fields.XHTMLField()
     assigned_users = models.ManyToManyField('UserProfile', blank=True, null=True)
     status = models.CharField(choices=NOTE_STATUS_CHOICES, max_length=1, default='1')
     topics = generic.GenericRelation('TopicAssignment')
     citations = generic.GenericRelation('Citation')
+    sections_counter = models.PositiveIntegerField(default=0)
     def has_topic(self, topic):
         return topic.id in self.topics.values_list('topic_id', flat=True)
     def as_text(self):
@@ -32,19 +36,51 @@ class Note(LastUpdateMetadata, Administered, URLAccessible, ProjectSpecific):
 
 class NoteSection(LastUpdateMetadata):
     u"""
-    A section of a note, consisting of a text field and, optionally, a reference
-    to a document and topic assignments.
+    The concrete base class for any note section.
     """
     note = models.ForeignKey(Note, related_name='sections')
-    document = models.ForeignKey('Document', blank=True, null=True)
-    content = fields.XHTMLField(blank=True, null=True)
+    note_section_id = models.PositiveIntegerField(blank=True, null=True)
+    ordering = models.PositiveIntegerField(blank=True, null=True)
     topics = generic.GenericRelation('TopicAssignment')
+    objects = InheritanceManager()
     class Meta:
         app_label = 'main'
-    def has_content(self):
-        return self.content is not None
-    def get_all_related_topics(self):
-        # Note sections can have their own topic assignments, but also inherit
-        # the topic assignments of the parent note
-        return set( [ta.topic for ta in self.note.topics.all()] +
-                    [ta.topic for ta in self.topics.all()] )
+        ordering = ['ordering', 'note_section_id']
+        #unique_together = ['note', 'note_section_id']
+    def save(self, *args, **kwargs):
+        n = self.note
+        save_note = False
+        if not self.note_section_id:
+            save_note = True
+            self.note_section_id = n.sections_counter = n.sections_counter + 1
+        super(NoteSection, self).save(*args, **kwargs)
+        if save_note:
+            n.save()
+
+class CitationNS(NoteSection):
+    document = models.ForeignKey('Document')
+    content = fields.XHTMLField(blank=True, null=True)
+    section_type = 'citation'
+    class Meta:
+        app_label = 'main'
+    def __unicode__(self):
+        return 'Note section -- citation -- {}'.format(self.document)
+
+class TextNS(NoteSection):
+    content = fields.XHTMLField()
+    section_type = 'text'
+    class Meta:
+        app_label = 'main'
+    def __unicode__(self):
+        content_str = etree.tostring(self.content)
+        return 'Note section -- text -- {}'.format(
+            content_str[:20] + '...' if len(content_str) > 20 else '')
+
+class NoteReferenceNS(NoteSection):
+    note_reference = models.ForeignKey(Note)
+    content = fields.XHTMLField(blank=True, null=True)
+    section_type = 'note_reference'
+    class Meta:
+        app_label = 'main'
+    def __unicode__(self):
+        return 'Note section -- reference -- {}'.format(self.note_reference)
